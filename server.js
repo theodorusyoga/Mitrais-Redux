24cf512c6a97e23112e98970f397edfa1a0a2e44
Theodorus Yoga <theodorusyoga_ti09@mail.ugm.ac.id>
2016-12-05 15:42:18 +0700

    hi~

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if(process.env.NODE_ENV !== 'production') {
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.js')
+  response.sendFile(__dirname + '/index.html')
 });
 
 app.listen(PORT, function(error) {
